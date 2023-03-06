3d921f59f22e905e7ac902533997eecf6197dc6c
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2023-03-06 13:45:52 +0700

    readme

--- src/router/desa_router.js
+++ src/router/desa_router.js
@@ -4,6 +4,7 @@ const validatorMiddleware = require("../middleware/validator_middleware");
 const desaCreateValidator = require("../validator/desa_validator");
 const router = require("express")();
 
+router.get("/", desa_controller.get);
 router.use(jwtMiddleware);
 router.post(
   "/create",
@@ -14,6 +15,5 @@ router.post(
 
 router.put("/edit/:slug", desa_controller.edit);
 router.delete("/delete/:slug", desa_controller.delete);
-router.get("/", desa_controller.get);
 
 module.exports = { desa_router: router };
