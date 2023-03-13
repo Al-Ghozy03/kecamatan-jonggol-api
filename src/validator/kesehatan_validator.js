ab84908ca0a1f58e0f3034dc16f6cc2ea2ef9857
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2023-03-13 15:05:11 +0700

    readme

--- src/validator/kesehatan_validator.js
+++ src/validator/kesehatan_validator.js
@@ -3,7 +3,6 @@ const { check } = require("express-validator");
 const kesehatanCreateValidator = [
   check("nama").isLength({ min: 1 }).withMessage("nama wajib diisi"),
   check("deskripsi").isLength({ min: 1 }).withMessage("deskripsi wajib diisi"),
-  check("web").isLength({ min: 1 }).withMessage("web wajib diisi"),
   check("maps").isLength({ min: 1 }).withMessage("maps wajib diisi"),
   check("kontak").isLength({ min: 12}).withMessage("kontak wajib 12 angka").isNumeric().withMessage("kontak harus angka"),
   check("id_desa").isLength({ min: 1 }).withMessage("id desa wajib diisi").isNumeric().withMessage("id desa harus angka"),
