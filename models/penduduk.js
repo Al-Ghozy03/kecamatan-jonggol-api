09d6b7a347032814ee2147379bae5d8c54018e8d
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2023-05-02 09:43:10 +0700

    new column penduduk

--- models/penduduk.js
+++ models/penduduk.js
@@ -63,6 +63,7 @@ module.exports = (sequelize, DataTypes) => {
       alamat_sekarang: DataTypes.STRING,
       id_desa: DataTypes.INTEGER,
       slug: DataTypes.STRING,
+      token: DataTypes.STRING,
     },
     {
       sequelize,
